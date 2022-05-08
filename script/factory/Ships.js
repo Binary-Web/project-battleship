19499e895bd99b2dcfd864a6841274e27f886ae8
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-05-07 23:12:02 -0700

    Ship Class

---
+++ script/factory/Ships.js
@@ -0,0 +1,22 @@
+class Ship {
+    constructor(name, hitBoxes) {
+        this.name = name;
+        this.hitBoxes = new Array(hitBoxes).fill("o");
+        this.isHorizontal = true;
+    }
+
+    hit(index) {
+        this.hitBoxes[index] = "x";
+    }
+
+    switchPosition() {
+        this.isHorizontal = !this.isHorizontal;
+    }
+
+    isSunk() {
+        if(this.hitBoxes.includes("o")) {
+            return false;
+        }
+    }
+
+}
