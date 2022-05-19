f44a1dee2bdaa23e74f4205158b26d0aa2abf5ef
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-05-18 23:23:43 -0700

    reset

--- script/index.js
+++ script/index.js
@@ -1,10 +1,1 @@
 import Player from './Player.js'
-document.querySelector('.inputPlayerName').addEventListener('submit', (e) => {
-    e.preventDefault()
-    const p1 = new Player(document.querySelector('.nameText').value);
-    const modal = document.querySelector('.modal-bg');
-    const main = document.querySelector('.main');
-
-    main.append(p1.createShipSelection());
-    main.append(p1.createBoard());
-})
