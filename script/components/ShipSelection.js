4d3b023add732c24a6cea0ffb0305cf1e03c8f02
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-05-19 23:22:41 -0700

    ui update

---
+++ script/components/ShipSelection.js
@@ -0,0 +1,5 @@
+import createShips from '../factory/createShips.js';
+
+export default function shipSelection(fiveShips) {
+
+}
--- script/index.js
+++ script/index.js
@@ -1,1 +1,9 @@
 import Player from './Player.js'
+document.querySelector('.inputPlayerName').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const container = document.querySelector('.modal-bg')
+    const playerName = document.querySelector('.nameText').value
+    const player = new Player(playerName);
+    container.append(player.shipSelection())
+    container.append(player.board)
+})
