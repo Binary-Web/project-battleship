418a142f31138e2338a5f4be6419629f8ad0bcc3
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-05-19 23:21:38 -0700

    ship.test

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -3,10 +3,16 @@ import Ship from '../script/factory/Ship'
 const ship1 = new Ship("TESTING", 5);
 
 test('ship1 getstatus', () => {
-    expect(ship1.getStatus()).toStrictEqual({Name: "TESTING", Health: ["o", "o", "o", "o", "o",]})
+    ship1.placeShip([1,2,3,4,5])
+    expect(ship1.getStatus()).toStrictEqual({Name: "TESTING", Health: ["o", "o", "o", "o", "o"],Position: "Horizontal", Location: [1,2,3,4,5]})
 })
 
 test('ship1 hit in the middle', () => {
     ship1.hit(2)
-    expect(ship1.getStatus()).toStrictEqual({Name: "TESTING", Health: ["o", "o", "x", "o", "o",]})
+    expect(ship1.getStatus()).toStrictEqual({Name: "TESTING", Health: ["o", "o", "x", "o", "o",], Position: "Horizontal", Location: [1,2,3,4,5]})
+})
+
+test('ship1 rotate', () => {
+    ship1.switchPosition()
+    expect(ship1.getStatus()).toStrictEqual({Name: "TESTING", Health: ["o", "o", "x", "o", "o",], Position: "Vertical", Location: [1,2,3,4,5]})
 })
